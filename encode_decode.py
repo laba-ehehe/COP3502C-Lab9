66e16e912c83c6487e91f0d4b7b2b5bf794ae072
laba-ehehe <donguyenlananh29052005@gmail.com>
2024-04-10 21:48:27 -0400

    ma m

--- encode_decode.py
+++ encode_decode.py
@@ -1,19 +1,23 @@
-def encode(password):
+def encode(password): # encode password function
     encode = ''
     for char in password:
         encoded_char = chr(ord(char) + 1)
         encode += encoded_char
     return encode
 
-while True:
-    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
-    choice = int(input('Please enter an option: '))
-    if choice == 3:
-        break
-    else:
-        password = input('Please enter your password to encode: ')
-        if choice == 1:
-            encode = encode(password)
-            print('Your password has been encoded and stored!')
-        elif choice == 2:
-            print('The encoded password is {encode}, and the original password is {password}.')
+def main():
+    while True:
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+        choice = int(input('Please enter an option: '))
+        if choice == 3:
+            break
+        else:
+            password = input('Please enter your password to encode: ')
+            if choice == 1:
+                encode = encode(password)
+                print('Your password has been encoded and stored!')
+            elif choice == 2:
+                print('The encoded password is {encode}, and the original password is {password}.')
+
+if __name__ == '__main__':
+    main()
